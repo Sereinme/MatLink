e8e6041c6c7b8c1a56959c9a9bc4125a2c896bac
Wenhao Li <sereinme@foxmail.com>
2025-02-20 20:51:59 +0800

    Initial Commit

---
+++ MatLink/MatLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatLinkApp.swift
+//  MatLink
+//
+//  Created by 李文昊 on 2025/2/20.
+//
+
+import SwiftUI
+
+@main
+struct MatLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MatLinkTests/MatLinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  MatLinkTests.swift
+//  MatLinkTests
+//
+//  Created by 李文昊 on 2025/2/20.
+//
+
+import Testing
+@testable import MatLink
+
+struct MatLinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
